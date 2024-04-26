b32ee40907beb3b5a0ac0ff3937c9da01949134f
k6av <43968691+k6av@users.noreply.github.com>
2024-04-26 23:08:23 +0200

    style: improve comments in `userpass` generator

--- src/generate/userpass.rs
+++ src/generate/userpass.rs
@@ -3,20 +3,23 @@ const USERNAME_CHARSET: &str = "qwertyuiopasdfghjklzxcvbnm1234567890";
 const PASSWORD_CHARSET: &str = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXVBNM~`!@#$%^&*()-_+=[]{}|;:'<,>./?";
 
 pub fn generate(mut rng: impl rand::RngCore + rand::CryptoRng, (user_l, pass_l): (usize, usize)) -> (String, String) {
-    let mut user_a = vec![0 as u8; user_l].into_boxed_slice();
-    rng.fill_bytes(&mut user_a);
+    // Generate username
+    let mut user_a = vec![0 as u8; user_l].into_boxed_slice(); // Initialize buffer slice
+    rng.fill_bytes(&mut user_a); // Fill slice with random bytes
 
-    let mut user_s = String::new();
+    let mut user_s = String::new(); // Initialize string
     let user_cl = USERNAME_CHARSET.len();
-    for c in user_a.into_iter() {
+    for c in user_a.into_iter() { // Map byte to character
         let c = *c as usize;
+        // TODO: find better mapping algorithm
         let idx = c % user_cl;
 
+        // FIXME
         user_s += USERNAME_CHARSET.as_bytes()[idx]
             .as_ascii().unwrap().as_str();
     }
 
-
+    // Generate password
     let mut pass_a = vec![0 as u8; pass_l].into_boxed_slice();
     rng.fill_bytes(&mut pass_a);
 
@@ -24,7 +27,6 @@ pub fn generate(mut rng: impl rand::RngCore + rand::CryptoRng, (user_l, pass_l):
     let pass_cl = PASSWORD_CHARSET.len();
     for c in pass_a.into_iter() {
         let c = *c as usize;
-        // TODO: does this give a suffeciently uniform distribution?
         let idx = c % pass_cl;
 
         pass_s += PASSWORD_CHARSET.as_bytes()[idx]
@@ -32,4 +34,37 @@ pub fn generate(mut rng: impl rand::RngCore + rand::CryptoRng, (user_l, pass_l):
     }
 
     (user_s, pass_s)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::generate;
+
+    #[test]
+    /// Generated strings must be of the specified length
+    fn basic_length() {
+        let user_l = 45;
+        let pass_l = 82;
+
+        let rng = rand::thread_rng();
+
+        let (user_s, pass_s) = generate(rng, (user_l, pass_l));
+
+        assert_eq!(user_s.len(), 45);
+        assert_eq!(pass_s.len(), 82);
+    }
+
+    #[test]
+    /// Generated strings must only contain valid ASCII characters
+    fn ascii_only() {
+        let user_l = 45;
+        let pass_l = 82;
+
+        let rng = rand::thread_rng();
+
+        let (user_s, pass_s) = generate(rng, (user_l, pass_l));
+
+        assert!(user_s.is_ascii());
+        assert!(pass_s.is_ascii());
+    }
 }
